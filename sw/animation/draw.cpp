f1a8e3305b40bf90c39d50c367f5dbfb3f32ecc9
Mario Coppola <m.coppola@tudelft.nl>
2020-09-30 17:20:21 +0200

    fixed circles for triangle

--- sw/animation/draw.cpp
+++ sw/animation/draw.cpp
@@ -12,7 +12,6 @@ void draw::set_color_white(const float intensity)
 
 void draw::make_circle_coordinates(const float r, const int precision)
 {
-  glPushMatrix();
   float angle, x, y;
   for (int i = 0; i <= precision; i++) { // Resolution
     angle = 2 * M_PI * i / precision;
@@ -20,110 +19,89 @@ void draw::make_circle_coordinates(const float r, const int precision)
     x = (r * xrat) * sin(angle);
     glVertex2d(y, x);
   }
-  glPopMatrix();
 }
 
 void draw::data()
 {
-  glPushMatrix();
   glRasterPos2f((-3.9 / zoom_scale - center_x), (-3.9 / zoom_scale - center_y));
   std::stringstream ss;
   this->set_color_white(1.);
   ss << "Time[s]:" << simtime_seconds << " \t" << "Fitness: " << evaluate_fitness();
   glutBitmapString(GLUT_BITMAP_8_BY_13, (unsigned char *)ss.str().c_str());
-  glPopMatrix();
 }
 
 void draw::axis_label()
 {
-  glPushMatrix();
   glRasterPos2f(3.9 / zoom_scale - center_x, 0.1 / zoom_scale);
   glutBitmapString(GLUT_BITMAP_8_BY_13,
                    (unsigned char *)std::string("E").c_str());
   glRasterPos2f(0.1 / zoom_scale, 3.9 / zoom_scale - center_y);
   glutBitmapString(GLUT_BITMAP_8_BY_13,
                    (unsigned char *)std::string("N").c_str());
-  glPopMatrix();
 }
 
 void draw::agent_number(const uint16_t &ID)
 {
-  glPushMatrix();
   glRasterPos2f(-0.01, 0.035);
   set_color_white(1.0);
   std::stringstream ss;
   ss << (int)ID;
   glutBitmapString(GLUT_BITMAP_8_BY_13, (unsigned char *)ss.str().c_str());
-  glPopMatrix();
 }
 
 void draw::triangle(const float &scl, const float &orientation)
 {
-  glPushMatrix();
   glBegin(GL_POLYGON);
   glColor3ub(200, 000, 000); // Red
-  glVertex2f(-1. * scl * yrat,  1. * scl * xrat);
-  glVertex2f(-1. * scl * yrat, -1. * scl * xrat);
-  glVertex2f(2. * scl * yrat,  0. * scl * xrat);
+  glVertex2f(-1. * scl,  1. * scl);
+  glVertex2f(-1. * scl, -1. * scl);
+  glVertex2f(2. * scl,  0. * scl);
   glEnd();
-  set_color_white(1.);
-  glPopMatrix();
 }
 
 void draw::circle(const float &r)
 {
-  glPushMatrix();
   glBegin(GL_POLYGON);
   glColor3ub(200, 000, 000); // Red
   make_circle_coordinates(r, 10);
   glEnd();
-  set_color_white(1.);
-  glPopMatrix();
 }
 
 void draw::circle_loop(const float &r)
 {
-  glPushMatrix();
+  set_color_white(1.);
   glLineWidth(1);
   glBegin(GL_LINE_LOOP);
   make_circle_coordinates(r, 20);
   glEnd();
-  set_color_white(1.);
-  glPopMatrix();
 }
 
 void draw::line(const float &x, const float &y)
 {
-  glPushMatrix();
   glLineWidth(2.5);
   set_color_white(1.0);
   glBegin(GL_LINES);
   glVertex3f(0.0, 0.0, 0.0);
   glVertex3f(x * xrat, -y * yrat, 0);
   glEnd();
-  glPopMatrix();
 }
 
 void draw::line(const float &x, const float &y, const float &width)
 {
-  glPushMatrix();
   glLineWidth(width);
   set_color_white(1.0);
   glBegin(GL_LINES);
   glVertex3f(0.0, 0.0, 0.0);
   glVertex3f(x * xrat, -y * yrat, 0);
   glEnd();
-  glPopMatrix();
 }
 
 void draw::point()
 {
-  glPushMatrix();
   glPointSize(10.0);
   glBegin(GL_POINTS);
   glVertex3f(0, 0, 0);
   glEnd();
-  glPopMatrix();
 }
 
 void draw::axes()
@@ -137,7 +115,7 @@ void draw::axes()
   glEnable(GL_LINE_STIPPLE);
   set_color_white(0.5);
   glVertex3f(-1000., 0., 0.);
-  glVertex3f(1000.0, 0., 0.);
+  glVertex3f(1000., 0., 0.);
   glEnd();
 
   glBegin(GL_LINES);
@@ -167,19 +145,25 @@ void draw::segment(const float &x0, const float &y0, const float &x1, const floa
 void draw::agent(const uint16_t &ID, const float x, const float y, const float &orientation)
 {
   glPushMatrix();
-  glTranslatef(y * xrat, x * yrat, 0.); // ENU to NED
-  glRotatef(90.0 - rad2deg(orientation), 0., 0., 1);
+  glTranslatef(y * xrat, x * yrat, 0.); // Position
+  glRotatef(90. - rad2deg(orientation), 0., 0., 1); // Rotation
   s[ID]->animation(); // Uses the animation function defined by the agent in use
-  s[ID]->controller->animation(ID); // Draws additional stuff from the controller, such as sensors
-  // agent_number(ID);
   glPopMatrix();
+
+  glPushMatrix();
+  glTranslatef(y * xrat, x * yrat, 0.); // Position
+  glRotatef(90., 0., 0., 1.); // Rotation
+  s[ID]->controller->animation(ID); // Draws additional stuff from the controller, such as sensors
+  agent_number(ID);
+  glPopMatrix();
+
 }
 
 void draw::velocity_arrow(const uint16_t &ID, const float &x, const float &y, const float &v_x, const float &v_y)
 {
   glPushMatrix();
-  glTranslatef(y * xrat, x * yrat, 0.0); // ENU to NED
-  glRotatef(90., 0., 0., 1.);
+  glTranslatef(y * yrat, x * yrat, 0.0); // Position
+  glRotatef(90., 0., 0., 1.); // Rotation
   line(v_x, v_y);
   glPopMatrix();
 }
@@ -187,8 +171,8 @@ void draw::velocity_arrow(const uint16_t &ID, const float &x, const float &y, co
 void draw::food(const float &x, const float &y)
 {
   glPushMatrix();
-  glTranslatef(y * xrat, x * yrat, 0.);
-  glRotatef(90., 0., 0., 1.);
+  glTranslatef(y * xrat, x * yrat, 0.); // Position
+  glRotatef(90., 0., 0., 1.); // Rotation
   point();
   glPopMatrix();
 }
